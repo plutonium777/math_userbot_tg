f7ecdfbe921c01e237ecdca318ab5f88e5c0aaaf
Vladyslav Oreshkin <41757358+plutonium777@users.noreply.github.com>
2021-04-13 02:33:55 +0300

    More flexible code and UX

--- userbot.py
+++ userbot.py
@@ -1,21 +1,30 @@
+# In the current version only integer operations supported! 
 from pyrogram import Client, filters
+import re
 
 app = Client("my_account")
 
-@app.on_message()
+@app.on_message(filters.me)
 def echo(client, message):
-	splited = message.text.split()
-	if(splited[0] == "+"):
-		message.reply_text(float(splited[1]) + float(splited[2]))
-		message.delete()
-	if(splited[0] == "-"):
-		message.reply_text(float(splited[1]) - float(splited[2]))
-		message.delete()
-	if(splited[0] == "*"):
-		message.reply_text(float(splited[1]) * float(splited[2]))
-		message.delete()
-	if(splited[0] == "/"):
-		message.reply_text(float(splited[1]) / float(splited[2]))
-		message.delete()
+	result = re.findall(r'\w+[+-/*]\w+', message.text)
+	end_message = message.text
+	for i in range(len(result)):
+		splited = re.split(r'[+-/*]', result[i])
+		if result[i].find('+') != -1:
+			math_result = int(splited[0]) + int(splited[1])
+			end_message = end_message.replace(result[i], str(math_result))
+		if result[i].find('-') != -1:
+			math_result = int(splited[0]) - int(splited[1])
+			end_message = end_message.replace(result[i], str(math_result))
+		if result[i].find('*') != -1:
+			math_result = int(splited[0]) * int(splited[1])
+			end_message = end_message.replace(result[i], str(math_result))
+		if result[i].find('/') != -1:
+			math_result = float(splited[0]) / float(splited[1])
+			end_message = end_message.replace(result[i], "{:g}".format(math_result))
+		print("Expression " + result[i] + " was changed to " + "{:g}".format(math_result))
+
+	message.reply_text(end_message)
+	message.delete()
 
 app.run()
